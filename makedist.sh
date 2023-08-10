4867a49a82eb99a918aab0aac06a88f46f4033b5
Theo Niessink <theo@taletn.com>
2023-08-10 13:35:31 +0200

    IPlugExample: Added AAX to make macOS dist script.

--- makedist.sh
+++ makedist.sh
@@ -14,12 +14,17 @@ then
 	exit 1
 fi
 
+aaxplugin=/Library/Application\ Support/Avid/Audio/Plug-Ins/$bundle.aaxplugin
+
+sudo rm -rf "$aaxplugin/" || exit
 rm -rf ~/"Library/Audio/Plug-Ins/Components/$bundle.component/" || exit
 rm -rf ~/"Library/Audio/Plug-Ins/CLAP/$bundle.clap/" || exit
 rm -rf ~/"Library/Audio/Plug-Ins/VST/$bundle.vst/" || exit
 
 xcodebuild clean || exit
 
-for target in AU CLAP VST2; do
+for target in AAX AU CLAP VST2; do
 	xcodebuild -configuration Release ndebug=NDEBUG GCC_WARN_ABOUT_DEPRECATED_FUNCTIONS=NO -target $target install || exit
 done
+
+# /Applications/PACEAntiPiracy/Eden/Fusion/Current/bin/wraptool sign --customernumber MYCU-STOM-ERNU-MBER --customername "Tale" --productname "IPlug Example" --signid "9ca1bf1665add1b51891e59b1746d9ec67106e12" --dsigharden --dsig1-compat off --verbose --in "$aaxplugin" --out "$aaxplugin" || exit
